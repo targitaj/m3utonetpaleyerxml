70581817c9200ed591b582d1f541e2b97d66811d
dron <mosala@gmail.com>
2024-12-30 19:52:59 +0000

    fvdfv

---
+++ ScreenSatusAndroid/ScreenSatusAndroid/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+
+namespace ScreenSatusAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public static Intent startIntent;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            // Запуск сервиса
+            if (startIntent == null)
+            {
+                startIntent = new Intent(this, typeof(MyService));
+                StartService(startIntent);
+            }
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
