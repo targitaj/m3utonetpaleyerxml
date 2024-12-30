5746f662fc7ac97da1275d84541d64a23a74e36b
dron <mosala@gmail.com>
2024-12-30 19:46:17 +0000

    высвыс

---
+++ BreakingBet/BreakingBet/MainWindowModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Mvvm;
+
+namespace BreakingBet
+{
+    public class MainWindowModel : BindableBase
+    {
+        private string _percentageOfProfitabilityFrom = "7";
+        private string _percentageOfProfitabilityTo = "40";
+        private string _logText = string.Empty;
+
+        public string PercentageOfProfitabilityFrom
+        {
+            get => _percentageOfProfitabilityFrom;
+            set => SetProperty(ref _percentageOfProfitabilityFrom, value);
+        }
+
+        public string PercentageOfProfitabilityTo
+        {
+            get => _percentageOfProfitabilityTo;
+            set => SetProperty(ref _percentageOfProfitabilityTo, value);
+        }
+
+        public string LogText
+        {
+            get => _logText;
+            set => SetProperty(ref _logText, value);
+        }
+    }
+}
---
+++ BreakingBet/BreakingBet/WebBrowserHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using mshtml;
+
+namespace BreakingBet
+{
+    public static class WebBrowserHelper
+    {
+        public static async Task<HTMLDocument> GetDocument(this WebBrowser webBrowser)
+        {
+            HTMLDocument document = null;
+
+            while (document == null)
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    document = (HTMLDocument)webBrowser.Document;
+                });
+
+                Thread.Sleep(100);
+            }
+
+            return document;
+        }
+    }
+}
